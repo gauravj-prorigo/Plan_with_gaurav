bbd8018874db7e4c34921d08399a401f98ca642a
Gaurav <your@email.com>
2026-01-19 15:49:00 +0530

    "Todays changes 19-5-2025"

---
+++ ecommerce/src/subcomponenets/Profile_Modal.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FcBusinessman } from "react-icons/fc";
+import "../css/ProfileModal.css";
+
+const ProfileModal = ({ username, onLogout }) => {
+  return (
+    <div className="profile-modal-main">
+      <div className="profile-modal-box">
+        
+        <FcBusinessman className="profile-icon" />
+
+        <h3 className="profile-username">{username}</h3>
+
+        <button className="logout-btn" onClick={onLogout}>
+          Logout
+        </button>
+
+      </div>
+    </div>
+  );
+};
+
+export default ProfileModal;
